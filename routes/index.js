776ea840557429bb6fb1bd4ad9a06296f3a91975
Ashfaq <ashfaqnisar00@gmail.com>
2021-12-12 23:15:20 +0530

    Added all the deps

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require("path")
+var html_file = path.join("../public/index.html")
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.sendFile(html_file);
 });
 
 module.exports = router;
